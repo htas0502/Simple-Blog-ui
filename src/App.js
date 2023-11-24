f1937de297337337de54827dde212bfff3d20606
Nguyen Tuan Khanh <Hoangtuanhsang.5555@gmail.com>
2023-11-24 17:30:32 +0700

    Create Login Section.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // Import Pages
 import Home from './pages/Home';
+import Error from './pages/Error';
 import Login from './pages/Login';
 import CreatePost from './pages/CreatePost';
 // Import from Firebase
@@ -14,6 +15,8 @@ import { auth } from './firebase-config';
 
 
 function App() {
+
+  // localStorage.clear();
 
   const [isAuth, setIsAuth] = useState(false);
 
@@ -29,13 +32,21 @@ function App() {
       })
   }
 
+  function handleCreatePostNav() {
+    if(!isAuth) {
+      alert("Please Sign in to Create Posts!");
+    } else {
+      window.location.pathname = "/createpost";
+    }
+  }
+
   return (
     <div className="App">
       <Router>
         {/* Navigation */}
         <nav className="">
           <Link to="/">Home</Link>
-          <Link to="/createpost">Create Post</Link>
+          <Link onClick={handleCreatePostNav}>Create Post</Link>
           {/* if not login ==> show login btn */}
           {!isAuth && <Link to="/login">Login</Link>}
           {/* log out */}
@@ -45,8 +56,9 @@ function App() {
         {/* Pages */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/error" element={<Error />} />
+          <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
           <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
-          <Route path="/createpost" element={<CreatePost />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 
 function Login({ setIsAuth }) {
     let navigate = useNavigate();
+    localStorage.setItem("isAuth", false);
 
     const signInWithGoogle = () => {
         signInWithPopup(auth, provider)
